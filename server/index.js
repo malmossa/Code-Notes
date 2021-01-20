63d72a4bd6d3d57aa90b7e2eb19d6d78a5943c1a
malmossa <malmossa@gmail.com>
2021-01-19 20:35:02 -0800

    Completed

--- server/index.js
+++ server/index.js
@@ -2,3 +2,22 @@ import bodyParser from 'body-parser'
 import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://mansor:Dammam_1973@cluster0.3g2t1.mongodb.net/<dbname>?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then( () => {
+    app.listen( PORT, () => {
+      console.log(`Server running on port: ${PORT}`)
+    })
+  })
+  .catch( (error) => {
+    console.log(error.message)
+  })

edb8c9e20568e8da5c816ccd3ea8ccea9b7e31c0
malmossa <malmossa@gmail.com>
2021-01-21 19:06:09 -0800

    Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,33 @@
 import React from 'react';
-import { Container, AppBar, Typography, Grid, Grow } from '@material-ui/core';
+import { Container, AppBar, Typography, Grid, Grow, ThemeProvider } from '@material-ui/core';
 
+
+import Form from './components/Form/Form';
+import Notes from './components/Notes/Notes';
 import logo from './images/code-notes-logo.png';
+import CustomTheme from './CustomTheme';
 
 const App = () => {
   return (
-    <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography>CODE NOTES</Typography>
-        <img src={logo} alt="code notes" height="60" />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Notes />
+    <ThemeProvider theme={CustomTheme}>
+      <Container maxWidth="lg">
+        <AppBar position="static" color="primary">
+          <img src={logo} alt="code notes" height="60" width="180" />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+              <Grid item xs={12} sm={7}>
+                <Notes />
+              </Grid>
+              <Grid item xs={12} sm={4}>
+                <Form />
+              </Grid>
             </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+          </Container>
+        </Grow>
+      </Container>
+    </ThemeProvider>
   )
 };
 
---
+++ client/src/components/Notes/Note/Note.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Note = () => {
+  return (
+    <h2>One Note</h2>
+  )
+};
+
+export default Note;

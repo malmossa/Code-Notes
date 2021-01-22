c00ccef6a8d0f65311b51ad94fd4d19f3eecef37
malmossa <malmossa@gmail.com>
2021-01-22 11:49:43 -0800

    Completed

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-import { getNotes } from '../controllers/notes.js';
+import { getNotes, createNote } from '../controllers/notes.js';
 
 const router = express.Router();
 
 router.get('/', getNotes);
+router.post('/', createNote);
 
 export default router;

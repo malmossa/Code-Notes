fd93111860367964419af70dced439707664eaad
malmossa <malmossa@gmail.com>
2021-01-24 12:03:18 -0800

    Completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import { Container, AppBar, Typography, Grid, Grow, ThemeProvider } from '@material-ui/core';
+import React, { useEffect } from 'react';
+import { Container, Grid, Grow, ThemeProvider } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
+import { getNotes } from './actions/notes';
 import Header from './components/Header/Header';
 import Form from './components/Form/Form';
 import Notes from './components/Notes/Notes';
 import CustomTheme from './CustomTheme';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getNotes());
+  }, [dispatch]);
+
   return (
     <ThemeProvider theme={CustomTheme}>
       <Container maxWidth="lg">

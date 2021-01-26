e5a67d2e6269f7dd93f7148442c4290e615ae77c
malmossa <malmossa@gmail.com>
2021-01-26 10:03:00 -0800

    Completed

--- client/src/actions/notes.js
+++ client/src/actions/notes.js
@@ -3,7 +3,7 @@ import * as api from '../api';
 // Action Creators
 export const getNotes = () => async (dispatch) => {
   try {
-    const { data } = await api.fechNotes();
+    const { data } = await api.fetchNotes();
     dispatch({ type: 'FETCH_ALL', payload: data});
   } catch (error) {
     console.log(error.message);

f5b2798d082192c4b9cb342038d7a8a33a503897
malmossa <malmossa@gmail.com>
2021-02-07 11:46:44 -0800

    Completed

--- client/src/components/Posts/Post/styles.js
+++ client/src/components/Posts/Post/styles.js
@@ -9,12 +9,7 @@ export default makeStyles({
     height: 0,
     paddingTop: '56.25%',
   },
-  cardActions: {
-    padding: '0 16px 8px 16px',
-  },
-  icon: {
-    marginRight: '5px',
-  },
+
   tags: {
     marginTop: 12,
   },
@@ -31,7 +26,5 @@ export default makeStyles({
   expandOpen: {
     transform: 'rotate(180deg)',
   },
-  marginLeft: {
-    marginLeft: 10,
-  }
+
 })

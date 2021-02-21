cd56ea70f6f54deab4b21314bd8ba1f56e88f839
malmossa <malmossa@gmail.com>
2021-02-20 18:51:27 -0800

    make it better

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if(process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 
 
-mongoose.connect(process.env.CONNECTION_URL || 'mongodb://localhost:27017/social-recipes' , { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL || 'mongodb://localhost/social-recipes' , { useNewUrlParser: true, useUnifiedTopology: true })
   .then( () => {
     app.listen(PORT, () => {
       console.log(`Server running on port: ${PORT}`)

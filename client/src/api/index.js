828ee2653a2edbf6dd2538f2c97b20e5616ce05b
malmossa <malmossa@gmail.com>
2021-02-21 16:48:44 -0800

    make it better

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://social-recipes.herokuapp.com/' });
+const API = axios.create({ baseURL: 'http://localhost:5000' });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
